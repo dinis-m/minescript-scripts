67792deadffe51d543fb887d2c295116cf539ea7
Dinis <dinis.mangas@gmail.com>
2026-05-22 23:39:43 +0100

    Double interaction added if targeted block is a note_block

--- anti_afk.py
+++ anti_afk.py
@@ -1,8 +1,13 @@
 import minescript as m
 from time import sleep
 
+block = m.player_get_targeted_block(2)
+
 while True:
     m.player_press_use(True)
+    if block is not None and block.type.split('[')[0] == "minecraft:note_block": 
+        m.player_press_attack(True)
+        m.player_press_attack(False)
     sleep(0.4)
     m.player_press_use(False)
 
@@ -10,4 +15,5 @@ while True:
         print("killing job...")
         break
 
+sleep(0.5)
 m.execute("\\killjob -1")
